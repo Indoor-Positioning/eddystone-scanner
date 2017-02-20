d363abc7f0e9cffceed004c1c40f4f60bbee8957
Mike Milonakis <mmilonakis@atypon.com>
2017-02-20 20:35:40 +0200

    Experiment with beacon list fragment

--- app/src/main/java/com/mooo/sestus/eddystonescanner/ScanBeaconsActivity.java
+++ app/src/main/java/com/mooo/sestus/eddystonescanner/ScanBeaconsActivity.java
@@ -1,7 +1,8 @@
 package com.mooo.sestus.eddystonescanner;
 
 import android.Manifest;
-import android.annotation.TargetApi;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.le.BluetoothLeScanner;
@@ -10,13 +11,8 @@ import android.bluetooth.le.ScanResult;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.HandlerThread;
-import android.os.Looper;
 import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
 import android.support.design.widget.NavigationView;
 import android.support.v13.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -42,34 +38,17 @@ public class ScanBeaconsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_beacons);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        ActionBar ab = getSupportActionBar();
-        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
-        ab.setDisplayHomeAsUpEnabled(true);
-
+        addToolbar();
+        addActionBar();
 
         drawerLayout = (DrawerLayout) findViewById(R.id.activity_scan_beacons);
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         if (navigationView != null)
             addDrawerContent(navigationView);
-        cb = new ScanCallback() {
-            @Override
-            public void onScanResult(int callbackType, ScanResult result) {
-                Log.v(TAG, "OnScanResult : " +  String.valueOf(callbackType) + "  " + result);
-            }
-
-            @Override
-            public void onBatchScanResults(List<ScanResult> results) {
-                for (ScanResult result: results)
-                    Log.v(TAG, "onBatchScanResults : " +   result.toString());
-            }
-
-            @Override
-            public void onScanFailed(int errorCode) {
-                Log.v(TAG, "Scan failed with errorCode: " + String.valueOf(errorCode));
-            }
-        };
+        cb = createScanCallback();
+        if (savedInstanceState == null) {
+            addFragment(R.id.fragmentContainer, new BeaconListFragment());
+        }
     }
 
 
@@ -90,6 +69,17 @@ public class ScanBeaconsActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         stopScan();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // Open the navigation drawer when the home icon is selected from the toolbar.
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void stopScan() {
@@ -116,17 +106,6 @@ public class ScanBeaconsActivity extends AppCompatActivity {
         leScanner.startScan(cb);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                // Open the navigation drawer when the home icon is selected from the toolbar.
-                drawerLayout.openDrawer(GravityCompat.START);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
     private void addDrawerContent(NavigationView navigationView) {
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -150,5 +129,44 @@ public class ScanBeaconsActivity extends AppCompatActivity {
     private boolean hasPermission() {
         return(PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION));
+    }
+
+    @NonNull
+    private ScanCallback createScanCallback() {
+        return new ScanCallback() {
+            @Override
+            public void onScanResult(int callbackType, ScanResult result) {
+                Log.v(TAG, "OnScanResult : " +  String.valueOf(callbackType) + "  " + result);
+            }
+
+            @Override
+            public void onBatchScanResults(List<ScanResult> results) {
+                for (ScanResult result: results)
+                    Log.v(TAG, "onBatchScanResults : " +   result.toString());
+            }
+
+            @Override
+            public void onScanFailed(int errorCode) {
+                Log.v(TAG, "Scan failed with errorCode: " + String.valueOf(errorCode));
+            }
+        };
+    }
+
+    protected void addFragment(int containerViewId, Fragment fragment) {
+        final FragmentTransaction fragmentTransaction = this.getFragmentManager().beginTransaction();
+        fragmentTransaction.add(containerViewId, fragment);
+        fragmentTransaction.commit();
+    }
+
+
+    private void addActionBar() {
+        ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void addToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
